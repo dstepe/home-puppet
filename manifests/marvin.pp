059b4b39225b54ea9a453473ed4c4557952348bd
root <root@localhost.localdomain>
2016-06-12 07:44:40 -0400

    Add ssh

--- manifests/marvin.pp
+++ manifests/marvin.pp
@@ -2,4 +2,12 @@ node "marvin.tf5net.local" {
 
 	include user
 
+	class { 'ssh::server':
+		storeconfigs_enabled => false,
+		options => {
+			'PasswordAuthentication' => 'no',
+			'PermitRootLogin'        => 'no',
+			'Port'                   => [22, 2222],
+		}
+	}
 }
